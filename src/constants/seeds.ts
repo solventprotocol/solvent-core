9e423d18c3632f314f4619bfbc780ed70b892700
Dhrumil Mayur Mehta <36199749+mdhrumil@users.noreply.github.com>
2022-07-20 02:57:32 +0530

    Add swapState PDA and utils (#9)

--- src/constants/seeds.ts
+++ src/constants/seeds.ts
@@ -3,3 +3,4 @@ export const SOLVENT_AUTHORITY_SEED = Buffer.from("authority-seed");
 export const LOCKER_SEED = Buffer.from("locker-seed");
 export const DEPOSIT_SEED = Buffer.from("deposit-seed");
 export const FARMER_AUTHORITY_SEED = Buffer.from("farmer-authority-seed");
+export const SWAP_SEED = Buffer.from("swap-seed");
